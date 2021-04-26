6dca7fd39b60aef8f5c375f84745aa2c44291dc0
Gaurav <gaurav.gp91@gmail.com>
2021-04-26 15:52:22 +0530

    made appropriate changes

--- src/Imaginary.java
+++ src/Imaginary.java
@@ -1,69 +1,117 @@
-public class Imaginary
-{
-    public static void main(String[] args)
-    {
-        Complex one = new Complex();
-        Complex two = new Complex(2,3);
-        Complex three = new Complex();
-        Complex four = new Complex();
-        Complex five = new Complex();
+class Complex {
 
-        one.real = 6;
-        one.imaginary =10;
-
-        one.display();
-        two.display();
-
-        three.add(one,two);
-        three.display();
-
-        four.sub(one,two);
-        four.display();
-
-        five.mul(one,two);
-        five.display();
-
-    }
-}
-
-class Complex
-{
-    int real;
-    int imaginary;
-
-    Complex(int x,int y)
-    {
-        real = x;
-        imaginary = y;
-    }
+    float real, imaginary;
 
     Complex()
     {
-        real =0;
-        imaginary = 0;
     }
 
-    void display()
+    Complex(float tempReal, float tempImaginary)
     {
-        System.out.println(real+"+ i*"+imaginary);
+        real = tempReal;
+        imaginary = tempImaginary;
     }
 
-    void add(Complex obj1,Complex obj2)
+    Complex addComp(Complex C1, Complex C2)
     {
-        real = obj1.real+obj2.real;
-        imaginary = obj1.imaginary+obj2.imaginary;
-    }
 
-    void sub(Complex obj1,Complex obj2)
+        Complex temp = new Complex();
+
+        temp.real = C1.real + C2.real;
+
+        temp.imaginary = C1.imaginary + C2.imaginary;
+
+        return temp;
+    }
+    Complex subComp(Complex C1, Complex C2)
     {
-        real = obj1.real-obj2.real;
-        imaginary = obj1.imaginary-obj2.imaginary;
-    }
 
-    void mul(Complex obj1,Complex obj2)
+        Complex temp = new Complex();
+
+
+        temp.real = C1.real - C2.real;
+
+
+        temp.imaginary = C1.imaginary - C2.imaginary;
+
+
+        return temp;
+    }
+    Complex mulComp(Complex C1, Complex C2)
     {
-        real = obj1.real*obj1.real - obj1.imaginary*obj1.imaginary;
-        imaginary = obj1.imaginary*obj2.real+obj1.real* obj2.imaginary;
+
+        Complex temp = new Complex();
+
+        temp.real = C1.real *C2.real - C1.imaginary*C2.imaginary;
+
+        temp.imaginary = C1.imaginary * C2.real + C1.real*C2.imaginary;
+
+        return temp;
+    }
+    Complex divComp(Complex C1, Complex C2)
+    {
+
+        Complex temp = new Complex();
+        float deno = (C2.real*C2.real + C2.imaginary*C2.imaginary);
+        temp.real = (C1.real*C2.real + C1.imaginary*C2.imaginary)/deno;
+        temp.imaginary = -(C2.real*C1.imaginary - C1.real*C2.imaginary)/deno;
+
+        return temp;
     }
 
+}
+
+public class Imaginary {
+
+
+    public static void main(String[] args)
+    {
+
+        Complex C1 = new Complex(6, 4);
+
+        System.out.println("Complex number 1 : "
+                + C1.real + " + i"
+                + C1.imaginary);
+
+        Complex C2 = new Complex(4, 5);
+
+        System.out.println("Complex number 2 : "
+                + C2.real +"+ i"
+                + C2.imaginary);
+
+        Complex C3 = new Complex();
+
+        C3 = C3.addComp(C1, C2);
+
+        Complex C4 = new Complex();
+
+
+        C4 = C4.subComp(C1, C2);
+        System.out.println("Sum of complex numbers : "
+                + C3.real + " + i"
+                + C3.imaginary);
+        if(C4.imaginary>=0) {
+            System.out.println("Difference of Complex number: "
+                    + C4.real +" + i "
+                    +C4.imaginary );
+        }
+        else {
+            System.out.println("Difference of Complex number: "
+                    + C4.real +
+                    +C4.imaginary + "i");
+
+        }
+
+
+        Complex C5 = new Complex();
+        C5 = C5.mulComp(C1, C2);
+        System.out.println("Multiplication of complex numbers : "
+                + C5.real + " + "
+                + C5.imaginary+ "i");
+        Complex C6 = new Complex();
+        C6 = C6.divComp(C1, C2);
+        System.out.println("Division of complex numbers : "
+                + C6.real + " + "
+                + C6.imaginary+ "i");
+    }
 }
