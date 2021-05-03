9bbabb7ea07f24decf2750f66fcfa470bbe1d11d
Gaurav <gaurav.gp91@gmail.com>
2021-05-03 15:56:09 +0530

    Made necessary changes

--- src/RVCE/Csedept.java
+++ src/RVCE/Csedept.java
@@ -3,7 +3,12 @@ package RVCE;
 import CSE.* ;
 public class Csedept extends Thirdsem
 {
-
-
+    public void display()
+    {
+        super.Welcomemsg();
     }
-
+    public void Welcomemsg()
+    {
+        System.out.println("Welcome to 3rd sem");
+    }
+}
--- src/RVCE/runner.java
+++ src/RVCE/runner.java
@@ -1,11 +1,19 @@
 package RVCE;
 
 
+import CSE.Thirdsem;
 
 public class runner {
     public static void main(String[] args)
     {
+        Thirdsem b = new Thirdsem();
         Csedept a = new Csedept();
+        a.display();
         a.Welcomemsg();
+        b.Welcomemsg();
+        Thirdsem b1;
+        b1 = a;
+        ((Csedept)b1).Welcomemsg();
+
     }
 }
